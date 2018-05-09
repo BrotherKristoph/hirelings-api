a8615f426be91d328b16f14535f3436ed9f962e3
Chris Kraljevic <chris.kraljevic@gmail.com>
2018-05-08 22:01:16 -0500

    Remove cruft from index

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,9 @@
 const functions = require('firebase-functions');
-const firebase = require('firebase-admin');
 const cors = require('cors')({origin: true});
-firebase.initializeApp(functions.config().firebase);
 
 exports.testDb = functions.https.onRequest((req, res) => {
 
     return cors(req, res, () => {
         res.status(200).send('Hello World!');
-
-        // firebase.database().ref('testDb').once('value').then(snapshot => {
-        //     responseData = JSON.stringify(snapshot.val());
-        //     res.send(responseData);
-        // });
     });
 });
